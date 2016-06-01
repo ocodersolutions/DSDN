00ba79fcfc86b8a24bb0432b644db7a7306a5586
Phan Văn Huy Hoàng <pvhuyhoang166@gmail.com>
2016-06-01 17:31:56 +0700

    upadate new_detail.php

---
+++ frontend/news_detail.php
@@ -0,0 +1,97 @@
+<?php include "header.php";?>
+    <div class="row">
+        <div class="container contact-content" style="padding: 0">
+            <img src="img/news.jpg" alt="">   
+            <p class="contact_p">Tin Tức</p>
+        </div>
+
+    </div>
+    <div class="row page-default ">
+        <div class="container content">
+            <div class="breakum-line">
+                <a href="">Trang chủ</a>/<span> Tin tức </span>
+            </div>
+        
+            <div class="col-sm-8 block">
+                <div class="news_content">
+                    <p class="new_tittle block">Di dời ga Đà Nẵng tốn gần 10.000 tỷ đồng</p>
+                    <span class="block">Thứ 4, 01/06/2016 , 9:37:03</span>
+                    <p class="short_des block">Ngày 15/3, đại diện tư vấn của Ngân hàng thế giới (WB) có buổi làm việc với UBND TP Đà Nẵng về Dự án di dời ga đường sắt Đà Nẵng và tái phát triển đô thị.</p>
+                    <p>Dự án di dời ga Đà Nẵng và tái phát triển đô thị có tổng mức đầu tư 442,23 triệu USD (tương đương 9.932 tỷ đồng). Trong đó, dự kiến vay ODA từ WB 334 triệu USD, phần vốn còn lại trị giá 107 triệu USD được huy động từ nguồn ngân sách địa phương.</p>
+                    <p>Kinh phí dự trù cho các hạng mục ở giai đoạn I gồm: xây dựng tuyến đường sắt mới dài 16 km, xây mới ga khách Đà Nẵng, xây dựng 6 cầu vượt đường bộ (cầu Thanh Khê, cầu vượt quốc lộ 14B và 4 cầu vượt đường bộ) khoảng 7.000 tỷ đồng.</p>
+                    <p>Tại buổi làm việc, sau khi phân tích 4 phương án mà Ban Quản lý dự án đường sắt (PMUR thuộc Bộ GT-VT) đã đưa ra, đại diện WB chú trọng phương án 1A.</p>
+                    <p>Theo đơn vị này, đây là phương án phù hợp nhất vì phù hợp với quy hoạch, hướng phát triển và có sự kết nối đồng bộ với hệ thống hạ tầng trong tương lai.</p>
+                    <div class="text-center"><img src="img/tinanh/new1.jpg" alt="hình ảnh minh họa"></div>
+                    
+                    <p>"Nếu xây dựng theo phương án này, có thể tận dụng tối ưu tuyến đường sắt cũ, hành lang tuyến... để tái phát triển đô thị thông qua việc phát triển các phương tiện vận tải công cộng (BRT)", đại diện ngân hàng WB cho hay.</p>
+                    <p>Theo lãnh đạo đơn vị này, phương án trên cũng sẽ tạo kết nối giữa nhà ga mới với đô thị mới hình thành. Đây là cơ hội để tạo động lực thúc đẩy khu đô thị trung tâm phía Tây TP Đà Nẵng phát triển.</p>
+                    <p>Việc di dời ga hiện trạng đến vị trí mới tại quận Liên Chiểu đem lại những lợi ích như: hạn chế xe tải, xe hạng nặng, giảm ùn tắc giao thông..."Chúng tôi đã thông báo dự án đến các nhà tài trợ và điều phối. Ngân hàng mong muốn UBND TP Đà Nẵng có thống nhất cao trong phương án để tháng 4 có đáp án và tháng 6 sẽ triển khai", đại diện WB phát biểu.Đại diện lãnh đạo Sở GT-VT và Sở Xây dựng Đà Nẵng cũng cơ bản thống nhất theo hướng tuyến 1A mà WB đưa ra. Tuy nhiên, lãnh đạo hai Sở này đề nghị đơn vị tư vấn của WB nên đưa ra được hành lang tuyến cụ thể để biết diện tích trừ ra để sau này làm tuyến metro trên cao.</p>
+                    <p>Vị lãnh đạo này cũng cho hay, Bộ GT-VT đã có văn bản chốt phương án di dời ga đường sắt Đà Nẵng theo phương án 1A. Theo đó, phạm vi triển khai dự án bao gồm ga Đà Nẵng và công trình liên quan từ Km 750+000 đến Km806+000.Ga mới Đà Nẵng sẽ tách ra làm hai ga hành khách và hàng hóa. Ga hành khách mới có quy mô (giai đoạn I) 33 ha, đặt tại phường Hòa Minh, quận Liên Chiểu. Còn ga hàng hóa mới có quy mô 25 ha, đặt tại phường Hòa Hiệp Nam (gần vị trí đường Nguyễn Tất Thành nối dài, nằm trong khu đô thị mới Golden Hills City).</p>
+                    <p class="author pull-right">Mạnh Nguyên</p>
+                </div>
+            </div>
+                
+            <div class="col-sm-4 block">
+                 
+                <div class="tinanh">
+                    <span class="tittle">Tin ảnh</span>
+                    <div class="items">
+                    <a href=""><img src="img/tinanh/tin anh 1_.jpg" alt="thumbnail" title="Hoạt động công ty" class="img"></a><a href="#" title="Hoạt động công ty" class="newslink">Đà Nẵng: Sẽ sớm di dời ga đường sắt ra khỏi nội thành</a><br>
+                    <span class="info">32 photos | 26639 view</span>
+                    </div>
+                    <div class="items">
+                    <a href=""><img src="img/tinanh/tin anh 2_.jpg" alt="thumbnail" title="Hoạt động công ty" class="img"></a><a href="#" title="Hoạt động công ty" class="newslink">Triển lãm công nghệ khai khoáng</a><br>
+                    <span class="info">32 photos | 26639 view</span>
+                    </div>
+                    <div class="items">
+                    <a href=""><img src="img/tinanh/tin anh 3_.jpg" alt="thumbnail" title="Hoạt động công ty" class="img"></a>
+                    <a href="#" title="Hoạt động công ty" class="newslink">Ga đường sắt mới sẽ được đặt tại quận Liên Chiểu</a>
+                    <span class="info">32 photos | 26639 view</span>
+                    </div>
+                    <div class="items">
+                    <a href=""><img src="img/tinanh/post1.jpg" alt="thumbnail" title="Hoạt động công ty" class="img"></a><a href="#" title="Hoạt động công ty" class="newslink">Phương án di dời và xây dựng ga đường sắt Đà Nẵng</a><br>
+                    <span class="info">32 photos | 26639 view</span>
+                    </div>
+                    <div class="items">
+                    <a href=""><img src="img/tinanh/tin anh 2_.jpg" alt="thumbnail" title="Hoạt động công ty" class="img"></a><a href="#" title="Hoạt động công ty" class="newslink">Triển lãm công nghệ khai khoáng</a><br>
+                    <span class="info">32 photos | 26639 view</span>
+                    </div>
+
+                    <div class="link"><a href="" class="linkmore">Xem thêm </a></div>
+                </div>
+            </div>
+
+            
+            <div class="col-sm-4 block">
+                 
+                <div class="tinanh">
+                    <span class="tittle">Tin Thể thao</span>
+                    <div class="items">
+                    <a href=""><img src="img/tinanh/tin anh 1_.jpg" alt="thumbnail" title="Hoạt động công ty" class="img"></a><a href="#" title="Hoạt động công ty" class="newslink">Đà Nẵng: Sẽ sớm di dời ga đường sắt ra khỏi nội thành</a><br>
+                    <span class="info">32 photos | 26639 view</span>
+                    </div>
+                    <div class="items">
+                    <a href=""><img src="img/tinanh/tin anh 2_.jpg" alt="thumbnail" title="Hoạt động công ty" class="img"></a><a href="#" title="Hoạt động công ty" class="newslink">Triển lãm công nghệ khai khoáng</a><br>
+                    <span class="info">32 photos | 26639 view</span>
+                    </div>
+                    <div class="items">
+                    <a href=""><img src="img/tinanh/tin anh 3_.jpg" alt="thumbnail" title="Hoạt động công ty" class="img"></a>
+                    <a href="#" title="Hoạt động công ty" class="newslink">Ga đường sắt mới sẽ được đặt tại quận Liên Chiểu</a>
+                    <span class="info">32 photos | 26639 view</span>
+                    </div>
+                    <div class="items">
+                    <a href=""><img src="img/tinanh/post1.jpg" alt="thumbnail" title="Hoạt động công ty" class="img"></a><a href="#" title="Hoạt động công ty" class="newslink">Phương án di dời và xây dựng ga đường sắt Đà Nẵng</a><br>
+                    <span class="info">32 photos | 26639 view</span>
+                    </div>
+                    <div class="items">
+                    <a href=""><img src="img/tinanh/tin anh 2_.jpg" alt="thumbnail" title="Hoạt động công ty" class="img"></a><a href="#" title="Hoạt động công ty" class="newslink">Triển lãm công nghệ khai khoáng</a><br>
+                    <span class="info">32 photos | 26639 view</span>
+                    </div>
+                    <div class="link"><a href="" class="linkmore">Xem thêm </a></div>
+                </div>
+            </div>
+            
+        </div>
+    </div> 
+    
+    <?php include "footer.php";?>
