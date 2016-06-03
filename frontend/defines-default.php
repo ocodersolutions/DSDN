5897c8a84f8ffd4205caf53cb31f7c2042877c3d
lecanhvan <lcv360@gmail.com>
2016-06-03 09:43:18 +0700

    require correct path

--- frontend/defines-default.php
+++ frontend/defines-default.php
@@ -1,2 +1,4 @@
 <?php
 define('BASE_URL', 'http://localhost/DSDN/frontend/');
+//define('BASE_URL', 'http://localhost:8089/DSDN/frontend/');
+?>
--- frontend/header.php
+++ frontend/header.php
@@ -1,6 +1,5 @@
 <?php 
-require('defines.php');
-//define('BASE_URL', 'http://localhost:8089/ocoder/DSDN/frontend/');
+require('defines-default.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
