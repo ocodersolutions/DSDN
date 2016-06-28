5f7de84127ce9b1a61205d70d4fdf7de71247188
Phan Văn Huy Hoàng <pvhuyhoang166@gmail.com>
2016-06-28 16:29:06 +0700

    update img

--- public/plugins/ckeditor/config.js
+++ public/plugins/ckeditor/config.js
@@ -7,4 +7,12 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	var duong_dan= '/public/plugins/';
+    config.filebrowserBrowseUrl         =duong_dan+ 'ckfinder/ckfinder.html';
+	config.filebrowserImageBrowseUrl  	=duong_dan+ 'ckfinder/ckfinder.html?type=Images';
+    config.filebrowserFlashBrowseUrl 	=duong_dan+ 'ckfinder/ckfinder.html?type=Flash';
+    config.filebrowserUploadUrl 		=duong_dan+ 'ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files';
+    config.filebrowserImageUploadUrl 	=duong_dan+ 'ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images';
+    config.filebrowserFlashUploadUrl 	=duong_dan+ '/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash';
+
 };
